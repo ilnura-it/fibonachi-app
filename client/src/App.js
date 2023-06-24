fc018c82c721dd7de6e5c70ed490c7e9771fb527
ilnura <t.ilnura@gmail.com>
2023-06-24 07:41:03 -0600

    web-socket connection added

--- client/src/App.js
+++ client/src/App.js
@@ -7,27 +7,29 @@ import Fib from "./Fib";
 function App() {
   return (
     <Router>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className="App">
+        <header className="App-header">
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
-      </header>
-    <div>
-          <Route exact path="/" component={Fib} />
-          <Route path="/otherpage" component={OtherPage} />
-        </div>
+          <div>
+            <Route exact path="/" component={Fib} />
+            <Route path="/otherpage" component={OtherPage} />
+          </div>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+
+        </header>
+
       </div>
     </Router>
   );
